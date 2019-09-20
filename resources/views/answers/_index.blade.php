04beb41cb75190830469f4c130d910369ca5f449
cseshahriar <cse.shahriar.hosen@gmail.com>
2019-09-20 12:40:05 +0600

    best_answer_id removed

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -22,9 +22,9 @@
                             <a href="#"  title="This answer is not userful" class="vote-down off"><i class="fas fa-caret-down fa-3x"></i> </a>
 
 
-                            <a href="#"  title="Mark this answer as best answer" class="vote-accepted mt-2"><i class="fas fa-check fa-2x"></i> 
+                            <a href="#"  title="Mark this answer as best answer" class="{{ $answer->status }} mt-2"><i class="fas fa-check fa-2x"></i>  
                             </a>    
-                        </div> 
+                        </div>   
 
                         <div class="media-body">
                             {!! $answer->body_html !!}
